641b23cf4df7f102693711b9fce583291d9338c2
KAWACHI Takashi <kawachi@p-lucky.net>
2012-12-06 10:18:44 +0900

    Rest 1 hour for every 4 hours

--- lib/hip_work_time/normalizer.rb
+++ lib/hip_work_time/normalizer.rb
@@ -13,6 +13,12 @@ module HipWorkTime
       worked_secs = @work_times.inject(0) { |a, e| a + e.seconds }
       start_datetime = @work_times[0].start_datetime.change(hour: 10)
       end_datetime = start_datetime + worked_secs.seconds
+
+      # rest 1 hour for every 4 hours
+      duration_sec = ((end_datetime - start_datetime) * 24 * 60 * 60).to_i
+      rest_hours = duration_sec / (60 * 60 * 4)
+      end_datetime += rest_hours.hours
+
       [TimeRange.new(start_datetime, end_datetime)]
     end
   end
