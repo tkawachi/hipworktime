a0014980b16bdc3755b75795d3a2ee9ff3a83b35
KAWACHI Takashi <kawachi@p-lucky.net>
2013-10-02 14:58:04 +0900

    Modified Normalizer

--- lib/hip_work_time/normalizer.rb
+++ lib/hip_work_time/normalizer.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 require_relative 'time_range'
 module HipWorkTime
   class Normalizer
@@ -10,16 +12,44 @@ module HipWorkTime
     def normalize
       return [] if @work_times.nil? || @work_times.size == 0
 
-      worked_secs = @work_times.inject(0) { |a, e| a + e.seconds }
-      start_datetime = @work_times[0].start_datetime.change(hour: 10)
-      end_datetime = start_datetime + worked_secs.seconds
-
-      # rest 1 hour for every 4 hours
-      duration_sec = ((end_datetime - start_datetime) * 24 * 60 * 60).to_i
-      rest_hours = duration_sec / (60 * 60 * 4)
-      end_datetime += rest_hours.hours
+      end_datetime = start_datetime + worked_secs - missing_rest_secs + rest_secs
 
       [TimeRange.new(start_datetime, end_datetime)]
+    end
+
+    def worked_secs
+      @work_times.inject(0) { |a, e| a + e.seconds }.seconds
+    end
+
+    def start_datetime
+      @work_times[0].start_datetime.change(hour: 10)
+    end
+
+    # 入力忘れの休憩を推測して返す
+    def missing_rest_secs
+      x = rest_secs - interval_secs
+      if x < 0
+        0
+      else
+        x
+      end
+    end
+
+    def interval_secs
+      intervals = 0
+      prev_work_time = nil
+      @work_times.each do |work_time|
+        if prev_work_time
+          intervals += work_time.start_datetime - prev_work_time.end_datetime
+        end
+        prev_work_time = work_time
+      end
+      intervals * 24 * 60 * 60
+    end
+
+    def rest_secs
+      # 常に2時間休憩したとする
+      1.hours
     end
   end
 end
